d087b4b1185cf255dcd1927cffdcf2f245aeb851
bill <wcstrickland1@gmail.com>
2021-07-15 12:45:21 -0500

    basic express app with a form view working

---
+++ index.js
@@ -0,0 +1,44 @@
+// ************** REQUIREMENTS AND INTITIALIZATION *************
+
+const express = require('express');
+const methodOverride = require('method-override'); // import method override to allow put and other requests from body
+const morgan = require('morgan'); // import logging middleware
+const ejsMate = require('ejs-mate'); //import ejs engine allowing for layouts rather than partials
+const session = require('express-session'); // express session tool
+const helmet = require('helmet'); // security mw package
+const app = express(); // running app
+const path = require('path'); // import path module to get access to file paths
+app.set('views', path.join(__dirname, 'views')); // set view path
+app.set('view engine', 'ejs'); // set view engine
+app.engine('ejs', ejsMate); // add engine
+
+// ******************* MIDDLEWARES *********************
+
+app.use(express.urlencoded({ extended: true })); // middle ware that parses post requests payloads incoming via DOM body
+app.use(methodOverride('_method')); // middle ware that allows put request to be served via DOM body
+app.use(morgan('tiny')); // logging mw: console.logs request, route, response time
+app.use(express.static(path.join(__dirname, 'public'))); // serves static assets
+
+
+//************ ROUTES *******************
+
+
+// HOME
+app.get('/', (req, res, next) => {
+    res.render('questionaire');
+});
+
+
+
+// // ERROR HANDLER
+// app.use((err, req, res, next) => {
+//     const { status = 500 } = err;
+//     if (!err.message) err.message = 'Something went wrong...';
+//     res.status(status).render('error', { err });
+// });
+
+// LISTENER
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(` visit at: 127.0.0.1:${port}`);
+});
---
+++ questionaire.js
@@ -0,0 +1,296 @@
+modeule.exports.questionaire = {
+    strength: [
+        {
+            left: "Strongly disagree",
+            question: "I track my macronutrients calories",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I would have no problem carrying my family from a burning building",
+            right: "Strongly agree"
+        },
+        {
+            left: "I have trouble standing up",
+            question: "I can squat 1.75 times my bodyweight",
+            right: "That sounds easy!"
+        },
+        {
+            left: "That sounds really hard",
+            question: "I can bench press 1.25 times my bodyweight (or overhead press 0.75xBW)",
+            right: "That sounds easy!"
+        },
+        {
+            left: "That sounds really hard",
+            question: "I can perform 20 push-ups in a row",
+            right: "That sounds easy!"
+        },
+        {
+            left: "That sounds really hard",
+            question: "I can perform a 1 minute ab plank",
+            right: "That sounds easy!"
+        },
+        {
+            left: "That sounds really hard",
+            question: "I can perform 5 strict pull-ups in a row",
+            right: "That sounds easy!"
+        }
+    ],
+    cardio: [
+        {
+            left: "That sounds really hard",
+            question: "I can run one mile in under 8 minutes",
+            right: "That sounds easy!"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I get out of breath going upstairs",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I regularly spend 60 minutes per week performing cardio",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I hit 10,000 steps per day",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "My resting heartrate is below 80 BPM",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I smoke (cigarettes)",
+            right: "Strongly agree"
+        }
+    ],
+    nutrition: [
+        {
+            left: "Strongly disagree",
+            question: "I am in total control of my diet",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I eat primarily whole/unprocessed foods",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I know how many calories I consume",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I track my macronutrients/calories",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "There are no bad foods",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I often find myself binging on snack foods",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I drink alcohol frequently",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I am under 20% bodyfat",
+            right: "Strongly agree"
+        }
+    ],
+    mindset: [
+        {
+            left: "Strongly disagree",
+            question: "I feel that I am in control of my life",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I often hit obstacles that make me want to give up",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I'm in my current situation due to events out of my control",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I am satisfied with who I am",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I often feel like a failure",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I wish I could have more respect for myself",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I am optimistic about the future",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I have a clear sense of purpose in life",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I am frequently in touch with friends and family",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I know that I can change my situation",
+            right: "Strongly agree"
+        }
+    ],
+    mentalHealth: [
+        {
+            left: "Strongly disagree",
+            question: "I am totally happy with my life",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I often feel anxious or worried",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I am comfortable around others",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I often feel sad, unmotivated, or even suicidal",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I often have an impending sense of doom, as if something bad will happen to me or my loved ones",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I struggle to find things than are fun or find pleasure in things that used to make me happy",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I have very little energy or feel tired all the time",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I have trouble concentrating frequently, like I have brain fog",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I find myself detached or numb at times",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I like the way my body looks",
+            right: "Strongly agree"
+        }
+    ],
+    pain: [
+        {
+            left: "Strongly disagree",
+            question: "I have persistent pain during my daily activities",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I struggle to decrease pain despite trying a lot of different treatments",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I have pain that prevents me from progressing or exercising",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "Pain prevents me from enjoying activities with my family and friends",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I have a bad back, hip, shoulder, elbow, knee, or ankle (or something else)",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I am currently injured'",
+            right: "Strongly agree"
+        }
+    ],
+    stress: [
+        {
+            left: "Strongly disagree",
+            question: "I have a good work/life balance",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I have no problems sleeping at night",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I tend to place others needs above my own",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly agree",
+            question: "I have a hard time controlling my stress levels",
+            right: "Strongly disagree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I tend to bring work home with me (mentally or literally)",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I find myself being irritable with my family and friends",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I feel like life is piling up on me",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "My family is easy to get along with",
+            right: "Strongly agree"
+        },
+        {
+            left: "Strongly disagree",
+            question: "I have hobbies that keep me happy and motivated",
+            right: "Strongly agree"
+        }
+    ]
+}
