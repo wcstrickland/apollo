7cb4c08eaa865f6bb63f797d16720ff6b6ebb2f7
bill <wcstrickland1@gmail.com>
2021-07-16 01:17:16 -0500

    radar chart renders

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // ************** REQUIREMENTS AND INTITIALIZATION *************
 
 const express = require('express');
+const bodyParser = require('body-parser')
 const methodOverride = require('method-override'); // import method override to allow put and other requests from body
 const morgan = require('morgan'); // import logging middleware
 const ejsMate = require('ejs-mate'); //import ejs engine allowing for layouts rather than partials
@@ -8,7 +9,10 @@ const session = require('express-session'); // express session tool
 const helmet = require('helmet'); // security mw package
 const app = express(); // running app
 const path = require('path');
-const { q } = require('./questions')
+const { q } = require('./questions');
+const { request } = require('http');
+const jshare = require('jshare')
+const { bb, radar } = require('billboard.js')
 app.use(express.static(path.join(__dirname, 'public'))); // serves static assets
 app.set('views', path.join(__dirname, 'views')); // set view path
 app.set('view engine', 'ejs'); // set view engine
@@ -19,6 +23,7 @@ app.engine('ejs', ejsMate); // add engine
 app.use(express.urlencoded({ extended: true })); // middle ware that parses post requests payloads incoming via DOM body
 app.use(methodOverride('_method')); // middle ware that allows put request to be served via DOM body
 app.use(morgan('tiny')); // logging mw: console.logs request, route, response time
+app.use(jshare())
 app.use(express.static(path.join(__dirname, 'public'))); // serves static assets
 
 
@@ -29,6 +34,19 @@ app.use(express.static(path.join(__dirname, 'public'))); // serves static assets
 app.get('/', (req, res, next) => {
     res.render('questionaire', { q });
 });
+
+app.post('/', (req, res, next) => {
+    console.log(req.body)
+    results = {}
+    const adder = (acum, cur) => parseInt(acum) + parseInt(cur);
+    let cats = ["strength", "cardio", "mindset", "mentalHealth", "stress", "pain", "nutrition"]
+    for (cat of cats) {
+        results[cat] = req.body[cat].reduce(adder)
+    }
+    res.locals.results = results
+    console.log(results)
+    res.render('show2', { results })
+})
 
 
 
