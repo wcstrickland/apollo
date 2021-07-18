ff18b1e842710b8face7e4b40ec30f32bd328a63
bill <wcstrickland1@gmail.com>
2021-07-18 00:43:24 -0500

    added intro page with image; adjusted responsiveness

---
+++ tips.js
@@ -0,0 +1,37 @@
+module.exports.tips = [
+    // strength NOTE these are ordered to sync with "cats" and are indexed with the same loop i variable
+    {
+        red: "Strength is more than just moving furniture. Poor strength can affect bone density, posture, and be a source of chronic pain. We specializing in building functional strength tailored to your needs.",
+        blue: "You are strong. Want to be really strong? Building muscle is a science often poor planning or lack of knowledge can hamstring even the most motivated person. Our strength programming can make sure you achieve your goals."
+    },
+    // cardio
+    {
+        red: "Out of breath? Poor cardio indicates the heart and lungs are not working at the level they should. Fortitude can create a plan for catching your breath for good.",
+        blue: "It appears you have good overall cardiovascular health. If you are looking to kick it up a notch. Maybe a 5k? We can help."
+    },
+    // Pain
+    {
+        red: "Chronic pain can 'spill' over into other areas of health such as sleep, and perpetuate a \"pain-guarding-pain\" cycle. We offer short term relief from pain as well as long term management solutions.",
+        blue: "It looks like you don't have a sever pain problem. We can give insight into reducing everyday pain and modifying the patterns that create it."
+    },
+    // Mental Health
+    {
+        red: "Looks like you may have signs of trouble with mental health. Fitness is more than just physicality. Problems with your body might be stemming from underlying issues that need to be addressed so you can live well.",
+        blue: "Mental health -- check! Something on your mind? Weighing you down. It might be time to check out tackling the thing thats holding you back."
+    },
+    // Stress
+    {
+        red: "Chronic poorly managed stress can lead to a myriad of health problems, affecting sleep, heart health, and weight. We offer guidance on reducing and managing stress in your life.",
+        blue: "Your current stress level looks good! Fortitude offers insight into the role stress might be playing with any injuries or chronic pain."
+    },
+    // Mindset
+    {
+        red: "It appears you might be looking at things in a sub-optimal way. Let us help you cultivate a mentality of achievement.",
+        blue: "Ever wonder about the mentality of an elite athlete? How they push themselves to incredible limits. Stop wondering. Find out."
+    },
+    // Nutrition
+    {
+        red: "You are what you eat. The food we consume lays the foundation for what our bodies can become. Poor diet affects every aspect of our health. Fortitude can supply you with knowledge to take control.",
+        blue: "Looking to be more than just 'not overweight'? Seeking to push your phsyique to the limit. Let us help."
+    }
+]
